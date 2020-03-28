ec320c0f49ce1446933b298375e79a7b4b19596f
Toddy <a35948001@163.com>
2020-03-28 21:41:46 +0800

    Q38,Q820,Q999

---
+++ simple/src/main/java/zerohandred/Q38.java
@@ -0,0 +1,68 @@
+package zerohandred;
+
+import org.junit.Test;
+
+/**
+ * 38. 外观数列
+ * <p>
+ * 「外观数列」是一个整数序列，从数字 1 开始，序列中的每一项都是对前一项的描述。前五项如下：
+ * <p>
+ * 1.     1
+ * 2.     11
+ * 3.     21
+ * 4.     1211
+ * 5.     111221
+ * 1 被读作  "one 1"  ("一个一") , 即 11。
+ * 11 被读作 "two 1s" ("两个一"）, 即 21。
+ * 21 被读作 "one 2",  "one 1" （"一个二" ,  "一个一") , 即 1211。
+ * <p>
+ * 给定一个正整数 n（1 ≤ n ≤ 30），输出外观数列的第 n 项。
+ * <p>
+ * 注意：整数序列中的每一项将表示为一个字符串。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 1
+ * 输出: "1"
+ * 解释：这是一个基本样例。
+ * 示例 2:
+ * <p>
+ * 输入: 4
+ * 输出: "1211"
+ * 解释：当 n = 3 时，序列是 "21"，其中我们有 "2" 和 "1" 两组，"2" 可以读作 "12"，也就是出现频次 = 1 而 值 = 2；类似 "1" 可以读作 "11"。所以答案是 "12" 和 "11" 组合在一起，也就是 "1211"。
+ * <p>
+ * Created by Toddy on 2020/3/28.
+ */
+public class Q38 {
+
+    @Test
+    public void test() {
+        System.out.println(countAndSay(10));
+    }
+
+    /**
+     *
+     */
+    public String countAndSay(int n) {
+        if (n == 1) {
+            return "1";
+        }
+        char[] chars = countAndSay(n - 1).toCharArray();
+        char st = chars[0];
+        int count = 1;
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < chars.length - 1; i++) {
+            if (chars[i] == chars[i + 1]) {
+                count++;
+            } else {
+                res.append(count);
+                res.append(st);
+                count = 1;
+                st = chars[i + 1];
+            }
+        }
+        res.append(count);
+        res.append(st);
+        return res.toString();
+    }
+}
